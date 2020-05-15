4f9b6d81c5adef7a00459dc098b19de765a5b36a
Matt Henkel <mhenkel@proofpoint.com>
2020-05-15 10:20:15 -0600

    Swap ALT and CMD keys.
    Also add yellow shine for layer 0, because.

--- keymap.c
+++ keymap.c
@@ -5,6 +5,17 @@
 #include "keymap_french.h"
 #include "keymap_spanish.h"
 #include "keymap_hungarian.h"
+#include "keymap_swedish.h"
+#include "keymap_br_abnt2.h"
+#include "keymap_canadian_multilingual.h"
+#include "keymap_german_ch.h"
+#include "keymap_jp.h"
+#include "keymap_bepo.h"
+#include "keymap_italian.h"
+#include "keymap_slovenian.h"
+#include "keymap_danish.h"
+#include "keymap_norwegian.h"
+#include "keymap_portuguese.h"
 
 #define KC_MAC_UNDO LGUI(KC_Z)
 #define KC_MAC_CUT LGUI(KC_X)
@@ -14,30 +25,46 @@
 #define KC_PC_CUT LCTL(KC_X)
 #define KC_PC_COPY LCTL(KC_C)
 #define KC_PC_PASTE LCTL(KC_V)
+#define ES_LESS_MAC KC_GRAVE
+#define ES_GRTR_MAC LSFT(KC_GRAVE)
+#define ES_BSLS_MAC ALGR(KC_6)
+#define NO_PIPE_ALT KC_GRAVE
+#define NO_BSLS_ALT KC_EQUAL
+#define LSA_T(kc) MT(MOD_LSFT | MOD_LALT, kc)
 
 enum custom_keycodes {
-  RGB_SLD = SAFE_RANGE, // can always be here
-  TOGGLE_LAYER_COLOR,
-  EPRM,
+  RGB_SLD = EZ_SAFE_RANGE,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [0] = LAYOUT_ergodox(KC_MINUS,KC_1,KC_2,KC_3,KC_4,KC_5,KC_TRANSPARENT,KC_BSLASH,KC_Q,KC_W,KC_E,KC_R,KC_T,KC_TRANSPARENT,KC_TAB,KC_A,KC_S,KC_D,KC_F,KC_G,KC_LSHIFT,LCTL_T(KC_Z),KC_X,KC_C,KC_V,KC_B,KC_TRANSPARENT,KC_LGUI,KC_GRAVE,KC_LEFT,KC_RIGHT,LALT_T(KC_NO),KC_PGUP,KC_PGDOWN,KC_INSERT,KC_ENTER,KC_ESCAPE,LCTL_T(KC_NO),KC_TRANSPARENT,KC_6,KC_7,KC_8,KC_9,KC_0,KC_EQUAL,KC_LBRACKET,KC_Y,KC_U,KC_I,KC_O,KC_P,KC_RBRACKET,KC_H,KC_J,KC_K,KC_L,KC_SCOLON,KC_QUOTE,KC_BSLASH,KC_N,KC_M,KC_COMMA,KC_DOT,KC_SLASH,KC_RSHIFT,RALT_T(KC_NO),KC_UP,KC_DOWN,KC_TRANSPARENT,KC_RGUI,KC_HOME,KC_END,KC_DELETE,RCTL_T(KC_NO),KC_BSPACE,KC_SPACE),
-
+  [0] = LAYOUT_ergodox_pretty(
+    KC_MINUS,       KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_EQUAL,
+    KC_BSLASH,      KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_TRANSPARENT,                                 KC_LBRACKET,    KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_RBRACKET,
+    KC_TAB,         KC_A,           KC_S,           KC_D,           KC_F,           KC_G,                                                                           KC_H,           KC_J,           KC_K,           KC_L,           KC_SCOLON,      KC_QUOTE,
+    KC_LSHIFT,      KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,           KC_TRANSPARENT,                                 KC_BSLASH,      KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       KC_RSHIFT,
+    KC_LALT,        KC_GRAVE,       KC_LEFT,        KC_RIGHT,       KC_LGUI,                                                                                                        KC_RGUI,        KC_UP,          KC_DOWN,        KC_TRANSPARENT, KC_RALT,
+                                                                                                    KC_PGUP,        KC_PGDOWN,      KC_HOME,        KC_END,
+                                                                                                                    KC_INSERT,      KC_DELETE,
+                                                                                    KC_ENTER,       KC_ESCAPE,      LCTL_T(KC_NO),  RCTL_T(KC_NO),  KC_BSPACE,      KC_SPACE
+  ),
 };
+
+
+rgblight_config_t rgblight_config;
+bool disable_layer_color = 0;
 
 bool suspended = false;
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
-    case EPRM:
-      if (record->event.pressed) {
-        eeconfig_init();
-      }
-      return false;
     case RGB_SLD:
       if (record->event.pressed) {
         rgblight_mode(1);
+      }
+      return false;
+    case TOGGLE_LAYER_COLOR:
+      if (record->event.pressed) {
+        disable_layer_color ^= 1;
       }
       return false;
   }
@@ -82,6 +109,32 @@ uint32_t layer_state_set_user(uint32_t state) {
       default:
         break;
     }
+    switch (layer) {
+      case 0:
+        if(!disable_layer_color) {
+          rgblight_enable_noeeprom();
+          rgblight_mode_noeeprom(1);
+          rgblight_sethsv_noeeprom(42,255,252);
+        }
+        break;
+      default:
+        if(!disable_layer_color) {
+          rgblight_config.raw = eeconfig_read_rgblight();
+          if(rgblight_config.enable == true) {
+            rgblight_enable();
+            rgblight_mode(rgblight_config.mode);
+            rgblight_sethsv(rgblight_config.hue, rgblight_config.sat, rgblight_config.val);
+          }
+          else {
+            rgblight_disable();
+          }
+        }
+        break;
+    }
     return state;
 
 };
+
+void keyboard_post_init_user(void) {
+  layer_state_set_user(layer_state);
+}
